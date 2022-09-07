e4cec19312fa8c37aecb46846ef0775e4bbba5af
Davide Troise <davide@troise.net>
2022-09-07 12:21:40 +0200

    Initial Commit

---
+++ ToggleDemo/ToggleDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleDemoApp.swift
+//  ToggleDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ToggleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
